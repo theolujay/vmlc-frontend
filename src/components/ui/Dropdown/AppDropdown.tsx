25e0efee29c254dcd960277eed4b4a1027acaae4
afobaje <ikukoyidave@gmail.com>
2025-10-12 14:37:53 +0100

    worked on logout

--- src/components/ui/Dropdown/AppDropdown.tsx
+++ src/components/ui/Dropdown/AppDropdown.tsx
@@ -1,12 +1,13 @@
 import { LogOutIcon, ProfileIcon } from "@/components/General/GettingStarted/GettingStartedAssets";
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
 import { CaretDropdown } from "../SvgAsset/GeneralAsset";
+import useLogout from "@/hooks/useLogout";
 
 
 
 
 const AppDropdown = () => {
-	
+	const {onLogout}=useLogout()
 
 	return (
 		<DropdownMenu.Root>
@@ -24,7 +25,7 @@ const AppDropdown = () => {
 					className="min-w-[220px] rounded-lg bg-white p-[10px] shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),_0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)] will-change-[opacity,transform] data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade data-[side=right]:animate-slideLeftAndFade data-[side=top]:animate-slideDownAndFade"
 					sideOffset={5}
 				>
-                    <DropdownMenu.Item className="group relative flex  h-[25px]  items-center   leading-none  outline-none  ">
+                    <DropdownMenu.Item className="group relative flex cursor-pointer  h-[25px]  items-center   leading-none  outline-none  ">
 						<div className=" pr-5  ">
 							<ProfileIcon/>
 						</div>
@@ -38,7 +39,7 @@ const AppDropdown = () => {
 						
 					</DropdownMenu.Item> */}
                     <DropdownMenu.Separator className="m-[5px] h-px bg-violet6" />
-					<DropdownMenu.Item className="group relative flex text-[#D42620] h-[25px]  items-center   leading-none  outline-none  ">
+					<DropdownMenu.Item onClick={onLogout} className="group cursor-pointer relative flex text-[#D42620] h-[25px]  items-center   leading-none  outline-none  ">
 						<div className=" pr-5  ">
 							<LogOutIcon/>
 						</div>
