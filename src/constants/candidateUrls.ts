6bb4a8e308239434e4402fbcb56cb2328523e184
afobaje <ikukoyidave@gmail.com>
2025-10-13 09:48:58 +0100

    worked on get started page

--- src/constants/candidateUrls.ts
+++ src/constants/candidateUrls.ts
@@ -1,5 +1,6 @@
 export const candidateUrls={
     candidate_exams_dashboard:'/dashboard/candidate',
     get_leaderboard:'/load-leaderboard/',
-    LIST_CANDIDATES:'/candidates/'
+    LIST_CANDIDATES:'/candidates/',
+    ACCOUNT_MGT:'/account-management/'
 }
