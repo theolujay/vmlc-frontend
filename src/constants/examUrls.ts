7f4c26500ba54ba6a30a8824ce7538bafc701ce1
afobaje <ikukoyidave@gmail.com>
2025-10-22 08:07:00 +0100

    worked on session pagination

--- src/constants/examUrls.ts
+++ src/constants/examUrls.ts
@@ -1,6 +1,6 @@
 export const examUrls={
     take_exam:(id:number)=>`/exams/${id}/take-exam/`,
     create_exam:'/exams/',
-    list_exams:'/exams/',
+    list_exams:(id:number=1)=>`/exams/?page=${id}`,
     VIEW_QUESTIONS:(exam_id:number)=>`/exams/${exam_id}/questions/`
 }
--- src/hooks/useListExams.tsx
+++ src/hooks/useListExams.tsx
@@ -2,10 +2,19 @@ import { ExamPortal } from '@/services/examPortal.service'
 import { useQuery } from '@tanstack/react-query'
 import React from 'react'
 
-export default function useListExams() {
+export default function useListExams(id:number) {
   const {isPending,data}=useQuery({
-    queryKey:['list-exams'],
-    queryFn:ExamPortal.listExams
+    queryKey:['list-exams',id],
+    queryFn: () => ExamPortal.listExams(id),
+    // queryFn:ExamPortal.listExams
+    enabled:!!id
   })
   return {isPending,data}
 }
+
+
+
+
+
+
+
