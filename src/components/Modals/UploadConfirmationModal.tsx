417541e85abeca6b5297212ccbeb013307ff7aea
afobaje <ikukoyidave@gmail.com>
2025-11-15 01:17:28 +0100

    worked on publish function

--- src/components/Modals/UploadConfirmationModal.tsx
+++ src/components/Modals/UploadConfirmationModal.tsx
@@ -2,12 +2,13 @@ import AppDialog from '@/components/ui/Modals/AppDialog'
 import usePublishLeaderboard from '@/hooks/usePublishLeaderboard'
 import clsx from 'clsx'
 import { ModalConfirmationIcon } from '../Admin/AdminIcons'
+import Spinner from '../ui/spinner/spinner'
 
 export default function UploadConfirmationModal({ open, close }: Readonly<{ open: boolean, close: (close: boolean) => void }>) {
     function handleClose() {
         close(!open)
     }
-    const { onSubmit } = usePublishLeaderboard()
+    const { onSubmit,isPending } = usePublishLeaderboard(handleClose)
     return (
         <AppDialog open={open}>
             <div className="flex flex-col gap-3 p-4 bg-white items-center shadow-sm rounded-lg">
@@ -17,15 +18,17 @@ export default function UploadConfirmationModal({ open, close }: Readonly<{ open
                 <div className="flex gap-2 mt-4 w-full">
                     <button
                         onClick={handleClose}
-                        className="px-4 py-2 rounded-lg cursor-pointer border border-[#E4E7EC] text-gray-700"
+                        className="px-4 py-2 font-semibold rounded-lg cursor-pointer border border-[#E4E7EC] text-gray-700"
                     >
                         CANCEL
                     </button>
                     <button
                         onClick={onSubmit}
+                        disabled={isPending}
                         className={clsx("px-4 py-2 rounded-lg font-semibold cursor-pointer flex-1 text-white bg-[#3E4095]")}
                     >
-                        CONFIRM UPLOAD
+                        {isPending?<Spinner/>:'CONFIRM UPLOAD'}
+                        
                     </button>
                 </div>
             </div>
