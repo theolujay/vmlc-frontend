8202265736cb2c1307c7b5cd2d0126b3d0684930
afobaje <ikukoyidave@gmail.com>
2025-11-18 11:58:03 +0100

    improved login hook

--- src/hooks/useRegister.tsx
+++ src/hooks/useRegister.tsx
@@ -1,6 +1,7 @@
 import { AuthService } from "@/services/auth.service";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useMutation } from "@tanstack/react-query";
+import { AxiosError } from "axios";
 import { useRouter } from "next/navigation";
 import { useForm } from 'react-hook-form';
 import { toast } from "react-toastify";
@@ -95,8 +96,8 @@ export default function useRegister() {
             form.reset()
             router.push('/login')
         },
-        onError:()=>{
-          toast.error('Registration was not successful')
+        onError:(error:AxiosError<any>)=>{
+          toast.error(error?.response?.data.detail||'Registration was not successful')
         }
         
 
