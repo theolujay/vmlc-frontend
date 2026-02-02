bc7c3feac6b7facf23b579e3ee7296a77ef2c170
Joseph Ezekiel <theolujay@gmail.com>
2026-02-02 10:39:22 +0100

    fix: properly handle when paths o titles are invalid

--- src/hooks/useGetBreadCrumbs.tsx
+++ src/hooks/useGetBreadCrumbs.tsx
@@ -3,6 +3,7 @@ import { usePathname } from 'next/navigation';
 
 export default function useGetBreadCrumbs(): string[] {
     const pathName = usePathname()
+    if (!pathName) return [];
     const pathSegments = pathName.split('/').filter(Boolean);
     return pathSegments;
 }
