29578e7f6222e32b3feb1aa1a1bc4ca679ebf824
Joseph Ezekiel <theolujay@gmail.com>
2026-02-07 07:50:57 +0100

    feat: improve toast notification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import type { Metadata } from "next";
 import { Suspense } from "react";
 import "./globals.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import 'react-toastify/dist/ReactToastify.css';
 import {ToastContainer} from 'react-toastify'
 import AppErrorBoundary from "./AppErrorBoundary";
 import Spinner from "@/components/ui/spinner/spinner";
@@ -72,7 +73,18 @@ export default function RootLayout({
                 <Suspense fallback={<div className="grid w-full h-screen place-content-center"><Spinner/></div>}>
                   {children}
                 </Suspense>
-                <ToastContainer/>
+                <ToastContainer 
+                  position="top-right"
+                  autoClose={2000}
+                  hideProgressBar={false}
+                  newestOnTop
+                  closeOnClick
+                  rtl={false}
+                  pauseOnFocusLoss
+                  draggable
+                  pauseOnHover
+                  theme="light"
+                />
               </QueryProvider>
             </NotificationProvider>
           </AuthProvider>
