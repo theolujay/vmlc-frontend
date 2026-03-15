87a321d71595e62f4453ad33f63bafc566c3fe21
Joseph Ezekiel <theolujay@gmail.com>
2026-03-15 22:30:04 +0100

    chore: add staging config for heartbeat

--- src/utils/isDev.ts
+++ src/utils/isDev.ts
@@ -1,1 +1,4 @@
-export const isDev = () => process.env.NODE_ENV === "development";
+export const isDev = () => {
+  const nodeEnv = process.env.NODE_ENV as string;
+  return nodeEnv === "development" || nodeEnv === "staging";
+};
